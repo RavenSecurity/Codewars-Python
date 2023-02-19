45228e9f8f7f757cbab29711cb7e0ac37130c5fd
Rayan <ravenSec93@gmail.com>
2023-02-19 20:56:22 +0100

    19 Feb

---
+++ 8 kyu/Function 2 - squaring an argument.py
@@ -0,0 +1,6 @@
+def square(n):
+    return n * n
+    pass
+# The pass statement is used as a placeholder for future code. When the pass statement is executed, 
+# nothing happens, but you avoid getting an error when empty code is not allowed. Empty code is not 
+# allowed in loops, function definitions, class definitions, or in if statements.
